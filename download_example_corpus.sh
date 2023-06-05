06790d0b8c6425a1cd85c09e7574a71383065d89
Ubuntu <ubuntu@ip-172-31-11-4.ec2.internal>
2023-06-05 15:39:18 +0000

    Adding to README

--- download_example_corpus.sh
+++ download_example_corpus.sh
@@ -1,2 +1,2 @@
-sacrebleu -t wmt13 -l es-en --echo src > pig/corpora/wmt13/es-en/wmt13.es-en.es
-sacrebleu -t wmt13 -l es-en --echo ref > pig/corpora/wmt13/es-en/wmt13.es-en.en
+sacrebleu -t wmt13 -l es-en --echo src > corpora/wmt13/es-en/wmt13.es-en.es
+sacrebleu -t wmt13 -l es-en --echo ref > corpora/wmt13/es-en/wmt13.es-en.en
