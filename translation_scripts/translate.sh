857b4ef242424279ad2db635df0d5c3deedb5f5f
Andrew Wilkinson <AWilkinson@sorenson.com>
2023-06-28 18:55:26 -0400

    Adding edited corpus documents

--- translation_scripts/translate.sh
+++ translation_scripts/translate.sh
@@ -2,12 +2,10 @@
 
 SL=en
 TL=es
-#CORPUS=serial
-CORPUS=pizza
-#SRC_PATH="../corpora/$CORPUS/$SL-$TL/$CORPUS.$SL-$TL.$SL"
+CORPUS=netflix
 SRC_PATH="../corpora_preprocessed/$CORPUS/$SL-$TL/$CORPUS.$SL-$TL.$SL"
 
-SYSTEM_ARR=(aws deepl gt msft)
+SYSTEM_ARR=(aws deepl gt msft openai)
 
 for SYS_NAME in ${SYSTEM_ARR[@]}
 do
